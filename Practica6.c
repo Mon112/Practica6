2f72fd68d1d8015ee32d2498a64c6a67075b60d2
Mon112 <37351877+Mon112@users.noreply.github.com>
2018-06-08 11:03:02 -0500

    Codigo Final

--- Practica6.c
+++ Practica6.c
@@ -2,50 +2,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <DHT11.h>
+#use fast_io(a)
+#use fast_io(d)
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
+//contador de anillo respecto a los activadores de los displays para el control del uso y mostrado de los displays
+//timer0 con el despliegue de la memoria de video(displays)
 
-unsigned int unidad=0,decena =0, unidadHR=0, decenaHR = 0;
-unsigned int8 T_byte1, T_byte2, RH_byte1, RH_byte2;
-unsigned int8 Sum;
-//#define Dht11 PIN_c4 
+int cambio=0;
+int contador=1;
+int bcdSin[10]={126, 48,109,121, 51, 91, 95,112,127,123};
+int bcdCon[10]={254,176,237,249,179,219,223,240,255,251};
+unsigned int8 dato[9] ={0};
+#INT_TIMER0
+void sti_TIMER0(){
+   switch(contador){
+      case 1:
+         output_a(contador);
+         output_d(bcdSin[dato[contador]]);
+      break;
+      case 2:
+         output_a(contador);
+         output_d(bcdCon[dato[contador]]);
+      break;
+      case 4:
+         output_a(contador);
+         output_d(bcdSin[dato[contador]]);
+      break;
+      case 8:
+         output_a(contador);
+         output_d(bcdSin[dato[contador]]);
+      break;
+   }
+   contador*=2;
+   if(contador>8){
+      contador=1;
+   } 
+}
 
-
-void main(void) {
-   setup_oscillator(OSC_16MHZ);
-   set_tris_a(0x00);
-   set_tris_b(0x00);
-   set_tris_e(0x00);
-   
-   
-while (true) {
-   
-   Check_response();
-   output_a(0x0F);
-   Start_Signal();
-   if(Check_response())
-   {
-      //output_a(0x01);
-      RH_byte1 = Read_Data();
-      RH_byte2 = Read_Data();
-      T_byte1 = Read_Data();
-      T_byte2 = Read_Data();
-      Sum= Read_Data();
-      if(Sum == ((RH_byte1+RH_byte2+T_byte1+T_byte2))){
-         decenaHR = RH_byte1/10;
-         unidadHR = RH_byte1%10;
-         decena = t_byte1/10;
-         unidad = T_byte1%10;
-         output_d(decenaHR);   
-         output_a(0x01);
+#INT_RB
+void isr_RB(){
+   if(1==input(PIN_B4)){
+      cambio++;
+      if(cambio==2){
+         cambio=0;
       }
    }
-      
-   
-   
 }
 
+void main(void) {
+   set_tris_d(0x00);
+   set_tris_a(0x00);
+   set_tris_e(0x00); 
+   set_tris_b(0xff);
+   setup_timer_0(RTCC_INTERNAL | RTCC_DIV_128 | RTCC_8_BIT);
+   enable_interrupts(INT_TIMER0);
+   enable_interrupts(INT_RB);
+   enable_interrupts(GLOBAL);
+   
+   while (1) { 
+      Start_signal();
+      if(check_response()){                    
+         RhByte1 = Read_Data();          
+         RhByte2 = Read_Data();                 
+         TemByte1 = Read_Data();                   
+         TemByte2 = Read_Data();                     
+         CheckSum = Read_Data();                       
+         if(CheckSum == ((RhByte1 + RhByte2 + TemByte1 + TemByte2) )){
+            switch(cambio){
+               case 0:
+                  output_e(0x02);
+                  dato[1]=(int)TemByte1/10;
+                  dato[2]=(int)TemByte1%10;
+                  dato[4]=(int)TemByte2/10;
+                  dato[8]=(int)TemByte2%10;
+               break;
+               case 1:
+                  output_e(0x01);
+                  dato[1]=(int)RhByte1/10;
+                  dato[2]=(int)RhByte1%10;
+                  dato[4]=(int)RhByte2/10;
+                  dato[8]=(int)RhByte2%10;
+               break;
+            }
+         }
+      } 
+   }
 
-}
